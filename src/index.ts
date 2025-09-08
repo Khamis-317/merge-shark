9e5a385970fcd36fc8fdfa58946b7d263302f1e2
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-09-09 00:50:47 +0300

    chore: setup commander & resolve command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hi Sharks!');
+import { Command, Option } from '@commander-js/extra-typings';
+
+const program = new Command();
+
+program
+  .name('shark')
+  .description('AI-powered Git conflict resolver.')
+  .version('0.0.1')
+  .command('resolve')
+  .description('resolves the current conflict if any')
+  .addOption(
+    new Option('-r, --repo <repo>', 'path for the Git repository').default(
+      process.cwd(),
+      'current working directory'
+    )
+  )
+  .action((options) => {
+    console.log(`Resolving conflict in ${options.repo}`);
+  });
+
+await program.parseAsync(process.argv);
